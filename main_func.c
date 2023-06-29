3db9520bf6a8b6b9806446420b9acafe025869d4
SK Sahil <SK.Sahil@iskraemeco.com>
2023-06-29 19:17:36 +0530

    main_func.c

---
+++ main_func.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int fst_var = 10;
+
+int main()
+{
+    printf("%d is my fst_var\n",fst_var);
+
+    fst_var++;
+    --((fst_var++)++);
+
+    printf("%d is my snd fst_var\n",fst_var);
+
+    return 0;
+}
